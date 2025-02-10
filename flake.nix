6585c15b5b36260fcca36b73cb4529e8d1371544
Michael Buckley <michaelcbuckley@proton.me>
2025-02-10 15:21:15 -0500

    Added devshell and checks

--- flake.nix
+++ flake.nix
@@ -2,16 +2,22 @@
   description = "Commonly Re-used NixOS Modules";
 
   inputs = {
+    # User override is expected
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nix-devshells = {
+      url = "github:Michael-C-Buckley/nix-devshells";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
+  outputs = {self, nixpkgs, nix-devshells}: {
     nixosModules = {
       nvidia = import ./hardware/nvidia.nix;
       libvirt = import ./virtualization/libvirt.nix;
     };
+
+    checks = nix-devshells.checks;
+    devShells.x86_64-linux.default = nix-devshells.devShells.x86_64-linux.nixos;
   };
 }
