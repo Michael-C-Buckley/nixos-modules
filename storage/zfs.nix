68799c0fcdeb0a06ba9bb0e3380192db2707d5f6
Michael Buckley <michaelcbuckley@proton.me>
2025-02-10 15:46:25 -0500

    Added options to ZFS

--- storage/zfs.nix
+++ storage/zfs.nix
@@ -1,17 +1,33 @@
-{pkgs, lib, ...}: {
-  boot = {
-    kernelModules = ["zfs"];
-    supportedFilesystems = ["zfs"];
-    zfs = {
-      devNodes = "/dev/disk/by-partuuid";
-      forceImportRoot = false;
-      requestEncryptionCredentials = lib.mkDefault false;
+{config, pkgs, lib, ...}: {
+
+  options.custom.zfs = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable ZFS features on host.";
+    };
+    encryption = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Request decryption credentials on boot.";
     };
   };
 
-  services.zfs.autoScrub.enable = true;
+  config = {
+    boot = {
+      kernelModules = ["zfs"];
+      supportedFilesystems = ["zfs"];
+      zfs = {
+        devNodes = "/dev/disk/by-partuuid";
+        forceImportRoot = false;
+        requestEncryptionCredentials = config.custom.zfs.encryption;
+      };
+    };
 
-  environment.systemPackages = with pkgs; [
-    zfs
-  ];
+    services.zfs.autoScrub.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      zfs
+    ];
+  };
 }
