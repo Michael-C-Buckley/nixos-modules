fb76be6fb1695a288db0a6e6311402ff85fda14e
Michael Buckley <michaelcbuckley@proton.me>
2025-02-10 15:55:43 -0500

    Added options to Libvirt

--- virtualization/libvirt.nix
+++ virtualization/libvirt.nix
@@ -1,31 +1,47 @@
-# Libvirt Common
-# - Users may need to be added to KVM group
-# - Assumes `br0` exists as the bridge
-
-{pkgs, lib, ...}: let
+{config, pkgs, lib, ...}: let
+  virtCfg = config.custom.virtualisation.libvirt;
   ovmfList = [
     (pkgs.OVMF.override {
       secureBoot = true;
       tpmSupport = true;
     }).fd];
 in {
-  environment.systemPackages = with pkgs; [
-    virt-viewer
-    virt-manager
-    tigervnc
-  ];
+  options.custom.virtualisation.libvirt = {
+    users = {
+      type = lib.types.list;
+      default = [];
+      description = "List of users to add the `KVM` group to.";
+    };
+    bridge = {
+      type = lib.types.str;
+      default = "br0";
+      description = "Name of the bridge device to bind for Libvirt.";
+    };
+  };
 
-  virtualisation.libvirtd = {
-    enable = true;
-    allowedBridges = lib.mkDefault ["br0"];
-    parallelShutdown = 5;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = ovmfList;
+  config = {
+    environment.systemPackages = with pkgs; [
+      virt-viewer
+      virt-manager
+      tigervnc
+    ];
+
+    users.users = lib.attrsets.genAttrs virtCfg.users (_: {
+      extraGroups = ["KVM"];
+    });
+
+    virtualisation.libvirtd = {
+      enable = true;
+      allowedBridges = lib.mkDefault config.options.customer;
+      parallelShutdown = 5;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = ovmfList;
+        };
       };
     };
   };
