c5fc9b655264c2b1812a03a83e9f322dae253d95
Michael Buckley <michaelcbuckley@proton.me>
2025-03-09 09:22:11 -0400

    Refined wireguard and added restarts on failures

--- network/wireguard-interface.nix
+++ network/wireguard-interface.nix
@@ -9,21 +9,22 @@
   description = "WireGuard: ${name}";
   wantedBy = ["multi-user.target"];
   serviceConfig = {
-    Type = "oneshot";
-    RemainAfterExit = true;
+    Type = "simple";
+
     ExecStartPre = [
       "${pkgs.iproute2}/bin/ip link add dev ${name} type wireguard"
       "${pkgs.iproute2}/bin/ip link set ${name} mtu ${toString mtu}"
-      # "${pkgs.wireguard-tools}/bin/wg setconf ${name} ${config.age.secrets."wg-${name}".path}"
       "${pkgs.wireguard-tools}/bin/wg setconf ${name} ${cfgPath}"
     ] ++ lib.concatMap (addr: [
       "${pkgs.iproute2}/bin/ip address add ${addr} dev ${name}"
     ])
     ipAddresses;
-    
-    ExecStart = "${pkgs.iproute2}/bin/ip link set ${name} up";
 
-    ExecStop = "${pkgs.iproute2}/bin/ip link delete ${name}";
-    OnFailure = "${pkgs.iproute2}/bin/ip link delete ${name}";
+    ExecStart = "${pkgs.iproute2}/bin/ip link set ${name} up";
+    ExecStopPost = "${pkgs.iproute2}/bin/ip link delete ${name}";
+    Restart = "on-failure";
+    RestartSec = "10s";
+    StartLimitIntervalSec = 60;
+    StartLimitBurst = 5;
   };
 }
