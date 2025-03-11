121741edf253684ed8b929b9aa01909065fe9d5d
Michael Buckley <michaelcbuckley@proton.me>
2025-03-11 17:03:12 -0400

    Fixed error since builtins didn't have a to Int function

--- network/address.nix
+++ network/address.nix
@@ -1,6 +1,6 @@
-_: cidr: let
+{lib}: cidr: let
   parts = builtins.split "/" cidr;
 in {
   address = builtins.elemAt parts 0;
-  prefixLength = builtins.toInt (builtins.elemAt parts 1);
+  prefixLength = lib.strings.toInt (builtins.elemAt parts 1);
 }
